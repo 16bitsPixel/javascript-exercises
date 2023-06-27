df0e88e2c4babb32a98e3b192afa50ff56d73a10
16bitsPixel <brandonllanes16@gmail.com>
2023-06-26 21:55:14 -0700

    optimized solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,5 @@
-const removeFromArray = function() {
-    if (arguments.length < 1) return "ERROR";
-    let arr = arguments[0];
-    for (let i = 1; i < arguments.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (typeof arr[j] == typeof arguments[i] && arr[j] == arguments[i]) {
-                arr.splice(j, 1);
-            }
-        }
-    }
-    return arr;
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
