b83185a2974d0d709579581b6e39c2123ec5e3f8
16bitsPixel <brandonllanes16@gmail.com>
2023-06-26 21:49:57 -0700

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    if (arguments.length < 1) return "ERROR";
+    let arr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (typeof arr[j] == typeof arguments[i] && arr[j] == arguments[i]) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
