19b284fe04c841623c27685487254c5cbf5ed389
16bitsPixel <brandonllanes16@gmail.com>
2023-06-26 21:24:57 -0700

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let output = "";
+    for (let i = 0; i < num; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
