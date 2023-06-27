d7b89ced29cfc415ed826bc91190710763c6711f
16bitsPixel <brandonllanes16@gmail.com>
2023-06-26 22:01:09 -0700

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    // edge cases
+    if (n < 0 || m < 0 || typeof n != "number" || typeof m != "number") return "ERROR";
 
+    let sum = 0;
+
+    // reorders n and m to be smaller first
+    if (m < n) {
+        let temp = n;
+        n = m;
+        m = temp;
+    }
+    
+    // adds up from n to m
+    for (let i = n; i <= m; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
