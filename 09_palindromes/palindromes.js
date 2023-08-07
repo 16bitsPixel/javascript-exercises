c692b3a480dfe946278365571597e82f69b10ea4
16bitsPixel <brandonllanes16@gmail.com>
2023-08-07 12:12:22 -0700

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = (string.replace(/[^a-zA-Z0-9]/g, "")).toLowerCase();
+    for (let i = 0; i < newString.length / 2; i++) {
+        if (newString[i] != newString[newString.length - 1 - i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
