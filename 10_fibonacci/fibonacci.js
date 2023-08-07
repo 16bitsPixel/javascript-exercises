2cf6f06884b40b7d8f70dba9d839bffab64ccc7f
16bitsPixel <brandonllanes16@gmail.com>
2023-08-07 12:24:19 -0700

    complete fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let fibPart = [0, 1];
+    for (let i = 1; i < num; i++) {
+        fibPart.push(fibPart[i] + fibPart[i - 1]);
+    }
+    return fibPart[num];
 };
 
 // Do not edit below this line
