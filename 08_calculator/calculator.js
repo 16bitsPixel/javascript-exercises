875e744d6a6127ca41764d9133be4440410466ff
16bitsPixel <brandonllanes16@gmail.com>
2023-08-07 12:06:58 -0700

    complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  arr.forEach(num => result += num);
+  return result;
 };
 
 const multiply = function() {
-
+  // case no arguments
+  if (arguments.length == 0) return 0;
+  let result = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    result *= arguments[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+	for (let i = 1; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  }
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
