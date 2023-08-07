21863f7f4a13b858902c7636a371b26b33903e0d
16bitsPixel <brandonllanes16@gmail.com>
2023-08-07 12:36:16 -0700

    complete final exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        let oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge > currentAge ? oldestPerson : currentPerson; 
+    });
 };
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
